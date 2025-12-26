3a3bdbaa2aed4b88fc0db2866829c64efedff59b
Kartikeya Ranjan <kartikeyaranjan02@gmail.com>
2025-12-26 16:24:44 +0530

    initialized spring

---
+++ src/main/java/com/kartikeya/apilayer/ApiLayerApplication.java
@@ -0,0 +1,13 @@
+package com.kartikeya.apilayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kartikeya/apilayer/ApiLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kartikeya.apilayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
